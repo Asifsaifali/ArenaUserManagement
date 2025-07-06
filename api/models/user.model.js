184548df598e4183549b979e813522646ad52ae5
Asifsaifali <asif263848@gmail.com>
2025-07-06 16:13:38 +0530

    created user model Schema

---
+++ api/models/user.model.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  chatId: {
+    type: Number,
+    required: true,
+  },
+  userId: {
+    type: Number,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    default: '',
+  },
+  username: {
+    type: String,
+    default: 'anonymous',
+  },
+  joinedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  subscription: {
+    startDate: {
+      type: Date,
+      default: Date.now,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['active', 'expired'],
+      default: 'active',
+    },
+  },
+});
+
+UserSchema.index({ chatId: 1, userId: 1 }, { unique: true });
+
+const User = mongoose.model('User', UserSchema);
+export default User;

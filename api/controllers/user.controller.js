108f413f38e10cab75394f232b3e7f97ae4aa0a8
Asifsaifali <asif263848@gmail.com>
2025-07-11 16:27:40 +0530

    added telegram user details to insert in thr db

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -4,26 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 const userRepository = new UserRepository();
 
-const TelegramNewMember = async () => {
+const Member = async (data) => {
   try {
-    const botToken = process.env.TELEGRAM_BOT_TOKEN;
-    const bot = new TelegramBot(botToken, { polling: true });
 
-    bot.on('polling_error', (error) => {
-  console.error('⚠️ Polling error:', error.code, error.message);
-});
-
-    bot.on("new_chat_members", (msg) => {
-      const chatId = msg.chat.id;
-
-      msg.new_chat_members.forEach(async(member) => {
+    for(const member of data.message.new_chat_members){
         const userId = member.id;
         const fullName = `${member.first_name} ${member.last_name || ""}`;
         const username = member.username || "anonymous";
-
-        console.log(`🟢New member joined: ${fullName}`);
-        console.log(`Username: @${username}`);
-        console.log(`Chat ID: ${chatId}, User ID: ${userId}`);
         const subscriptionDate = new Date();
         const subscriptionEndDate = new Date(subscriptionDate);
         subscriptionEndDate.setDate(subscriptionEndDate.getDate() + 30);
@@ -39,19 +26,12 @@ const TelegramNewMember = async () => {
             status: "active",
           },
         };
-
         const newUser = await userRepository.createUser(userData);
         if (!newUser) {
          console.log("🔴Error creating user in the database");
          ;
         }
-
-        bot.sendMessage(chatId, `👋Welcome ${fullName}! to the Group`);
-         console.log("🟢 Telegram new member handler is set up successfully");
-      });
-    });
-    
-   ;
+      }
   } catch (error) {
     console.log("🔴Error setting up Telegram new member handler:", error);
   }
@@ -75,4 +55,4 @@ const startBot = () => {
 
 
 
-export { TelegramNewMember}
+export { Member}

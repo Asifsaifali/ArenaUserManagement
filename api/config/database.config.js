11223138d20f1c78aba6bdbd0f21377cfdcedfdf
Asifsaifali <asif263848@gmail.com>
2025-07-12 16:31:19 +0530

    a new field added in model

--- api/config/database.config.js
+++ api/config/database.config.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config()
 const MONGO_URI = process.env.MONGO_URI 
+console.log(MONGO_URI);
 
 const connectDB = async()=>{
     const connect = await mongoose.connect(MONGO_URI)

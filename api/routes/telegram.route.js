4ec5ff7bd0121408114a0e23472c7353bbc7e74e
Asifsaifali <asif263848@gmail.com>
2025-07-13 16:34:12 +0530

    webhook routes to make sure telegram bot run every time

---
+++ api/routes/telegram.route.js
@@ -0,0 +1,8 @@
+import express from "express"
+import TelegramNewMember from "../controllers/telegram.controller.js"
+const router = express.Router()
+
+router.post('/webhook', TelegramNewMember)
+
+export default router;
+

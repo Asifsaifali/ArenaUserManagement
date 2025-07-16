9e40e90ff88f0c9282b9b6a0fe2cc0927be4e7d5
Asifsaifali <asif263848@gmail.com>
2025-07-16 17:18:37 +0530

    Added new Welcome message for user

--- api/controllers/telegram.controller.js
+++ api/controllers/telegram.controller.js
@@ -22,13 +22,16 @@ const TelegramNewMember = async (req, res) => {
         const userId = member.id;
 
         const welcomeMessage = `👋 Welcome ${name} to Our Official Arena Group`;
+        const welcomeUserMessage = `👋 Welcome to the Arena : ${name} 👨‍💻!
+                            Introduction to the arena : ${process.env.INTRO_TELEGRAM_CHANNEL}
+                            And please introduce yourself in the "INTRO SECTION".. So we get to know you better.`
 
         const response = await fetch(`${TELEGRAM_API}/sendMessage`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
             chat_id: chatId,
-            text: welcomeMessage,
+            text: welcomeUserMessage,
           }),
         });
 

77f73bb4a1ed5b8af3aae92d04d23adfec807ba3
Asifsaifali <asif263848@gmail.com>
2025-07-16 16:33:51 +0530

    changes the controller logic

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,44 +1,37 @@
-import UserRepository from "../repository/user.repository.js"
-import TelegramBot from "node-telegram-bot-api";
+import UserRepository from "../repository/user.repository.js";
 import dotenv from "dotenv";
 dotenv.config();
+
 const userRepository = new UserRepository();
 
-const UserDetails = async (req, res, next) => {
+export const insertNewUser = async (member, chatId) => {
   try {
-     const data = req.body
-     const chatId = data.message.chat.id
-    for(const member of data.message.new_chat_members){
-        const userId = member.id;
-        const username = member.username || "anonymous";
-        const subscriptionDate = new Date();
-        const subscriptionEndDate = new Date(subscriptionDate);
-        subscriptionEndDate.setDate(subscriptionEndDate.getDate() + 30);
-        const userData = {
-          chatId: chatId,
-          userId: userId,
-          firstName: member.first_name,
-          lastName: member.last_name || "",
-          username: username || "anonymous",
-          subscription: {
-            startDate: subscriptionDate,
-            endDate: subscriptionEndDate,
-            status: "active",
-          },
-        };
-        const newUser = await userRepository.createUser(userData);
-        if (!newUser) 
-         console.log("🔴Error creating user in the database");
-        console.log("New User data inserted in db: ",userData);
-        res.sendStatus(200)
-        next();
-      }
-  } catch (error) {
-    console.log("🔴Error setting up Telegram new member handler:", error);
+    const userId = member.id;
+    const username = member.username || "anonymous";
+    const subscriptionDate = new Date();
+    const subscriptionEndDate = new Date(subscriptionDate);
+    subscriptionEndDate.setDate(subscriptionEndDate.getDate() + 30);
+
+    const userData = {
+      chatId,
+      userId,
+      firstName: member.first_name,
+      lastName: member.last_name || "",
+      username,
+      subscription: {
+        startDate: subscriptionDate,
+        endDate: subscriptionEndDate,
+        status: "active",
+      },
+    };
+
+    await userRepository.createUser(userData);
+    console.log("✅ New User inserted:", userData);
+  } catch (err) {
+    if (err.code === 11000) {
+      console.log("⚠️ User already exists:", member.id);
+    } else {
+      console.error("❌ DB Insert Error:", err);
+    }
   }
 };
-
-
-
-
-export default UserDetails 

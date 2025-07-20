c53b193adda08f503172ed032a735795285f5e63
Asifsaifali <asif263848@gmail.com>
2025-07-20 15:55:15 +0530

    Define Admin model for db intraction

---
+++ api/models/admin.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const adminSchema = new mongoose.Schema({
+
+    username : {
+        type : String,
+        required : true,
+    },
+
+    password:{
+        type: String,
+        required: true,   
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    name :{
+        type: String,
+        required: true,
+    }
+}, { timestamps: true });
+
+const Admin = mongoose.model("Admin", adminSchema);
+export default Admin;

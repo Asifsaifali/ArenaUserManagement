d91751a7b29391589c2b945d0e4e604528065ccf
Asifsaifali <asif263848@gmail.com>
2025-07-29 23:57:42 +0530

    Renamed frontend to client and removed old frontend folder

--- frontend/prettier.config.mjs
+++
@@ -1,7 +0,0 @@
-// prettier.config.mjs
-import { tailwindPlugin } from "prettier-plugin-tailwindcss";
-
-export default {
-  plugins: [tailwindPlugin],
-  tailwindConfig: "./tailwind.config.cjs",
-};

590d41c5285a757ef6d4d5da5725b265460fb9d2
Asifsaifali <asif263848@gmail.com>
2025-08-01 16:51:11 +0530

    added admin routes

--- api/routes/admin.route.js
+++ api/routes/admin.route.js
@@ -1,9 +1,10 @@
-import { createAdmin } from "../controllers/admin.controller.js";
+import { createAdmin, adminLogin } from "../controllers/admin.controller.js";
 import express from "express";
 
 const router = express.Router();
 
 router.post("/signin", createAdmin)
+router.post("/login", adminLogin)
 
 export default router;
 

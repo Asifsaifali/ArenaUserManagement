0b65674249802fb08280b6e7df61cd6767a692ae
Asifsaifali <asif263848@gmail.com>
2025-08-06 16:51:16 +0530

    added routes in user section

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,10 +1,14 @@
-import { getTotalaUsers } from "../controllers/user.controller.js";
+import { bulkInsertUsers, TotalUsers, getTotalaUsers, fetchLatestUsers, MonthlyUsersStats, getGraphData } from "../controllers/user.controller.js";
 import express from "express";
 
 const router = express.Router();
 
 router.get('/users', getTotalaUsers);
-router.get('/active-users', getTotalaUsers); // Assuming this is the same controller for active users
-
+router.get('/active-users', TotalUsers); // Assuming this is the same controller for active users
+router.post('/all-users', bulkInsertUsers); 
+router.get('/total-users', getTotalaUsers); 
+router.get('/latest-users', fetchLatestUsers);
+router.get('/monthly-users-stats', MonthlyUsersStats);
+router.get('/graph-data',getGraphData)
 
 export default router;

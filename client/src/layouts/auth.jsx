c0092317ba8f7dcc60054a1818cf522eea1459dd
Asifsaifali <asif263848@gmail.com>
2025-08-08 17:03:42 +0530

    connected the login page to backend

--- client/src/layouts/auth.jsx
+++ client/src/layouts/auth.jsx
@@ -5,7 +5,6 @@ import {
   UserPlusIcon,
   ArrowRightOnRectangleIcon,
 } from "@heroicons/react/24/solid";
-import { Navbar, Footer } from "@/widgets/layout";
 import routes from "@/routes";
 
 export function Auth() {
@@ -35,13 +34,12 @@ export function Auth() {
   return (
     <div className="relative min-h-screen w-full">
       <Routes>
-        {routes.map(
-          ({ layout, pages }) =>
-            layout === "auth" &&
-            pages.map(({ path, element }) => (
-              <Route exact path={path} element={element} />
-            ))
-        )}
+        {routes
+          .filter((r) => r.layout === "auth")
+          .flatMap((r) => r.pages)
+          .map(({ path, element }, index) => (
+            <Route key={path || index} path={path} element={element} />
+          ))}
       </Routes>
     </div>
   );
